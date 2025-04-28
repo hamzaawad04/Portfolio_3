01d52d0e3eaca811ee36623b2212105838e2463c
Hamza Ashraf Bayoumy Ramadan Awad <171928546+hamzaawad04@users.noreply.github.com>
2025-04-28 20:06:35 +0100

    Update recipe-search.blade.php

--- laravkitchen/resources/views/livewire/recipe-search.blade.php
+++ laravkitchen/resources/views/livewire/recipe-search.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <input type="text" wire:model.live="searchTerm" class="form-control mb-4 w-50 mx-auto d-block" placeholder="Search recipes by name or type...">
+
+    <div class="row">
+        @forelse ($recipes as $recipe)
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="{{ asset('images/' . $recipe->image) }}" class="card-img-top" alt="{{ $recipe->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $recipe->name }}</h5>
+                        <p class="card-text">{{ $recipe->description }}</p>
+                        <a href="{{ url('/recipes/' . $recipe->rid) }}" class="btn btn-primary">View Recipe</a>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <p class="text-center">No recipes found for "{{ $searchTerm }}"</p>
+        @endforelse
+    </div>
+
+    <div class="d-flex justify-content-center mt-4">
+        {{ $recipes->links() }}
+    </div>
 </div>
